de9deef4492676e5ce740ae40a069d1c6f49c601
indiatechtimes <indiatechtimes@gmail.com>
2024-03-21 01:20:40 -0700

    initial updating

---
+++ lib/practice.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+  print("hello");
+  print("Please enter your name ");
+  var name= stdin.readLineSync();
+  print("hello this is $name how can i help you ");
+}
+
+
+
+
+
+
