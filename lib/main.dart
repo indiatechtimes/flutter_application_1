a4630b9f80a08fb2113a47f3726ac40192a68b85
indiatechtimes <indiatechtimes@gmail.com>
2024-03-23 21:34:07 -0700

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -33,10 +35,14 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
+  }//closing of Widget build(BuildContext context)
+}// closing of class MyApp extends StatelessWidget
 
-class MyHomePage extends StatefulWidget {
+
+
+
+
+class MyHomePage extends StatefulWidget {// This is my first intent(page)
   const MyHomePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -86,40 +92,41 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body:
+
+
+      /*Text('hello flutter devops',style: TextStyle(
+        color: Colors.red,
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        backgroundColor: Colors.indigo,
+      ),)*/
+
+
+      /*Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          color: Colors.red,
+          child: Center(child: Text('Hey Captain',style: TextStyle(fontSize: 25))),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )*/
+
+
+      //Center(child: Text('Hello Captain',style: TextStyle(fontSize: 30),)),
+      /*Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          color: Colors.red,
+
+          child: Center(child: Text('hello devops!',style: TextStyle(fontSize: 25),)),
+
+        ),
+      )*/
+
+
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/practice.dart
+++ lib/practice.dart
@@ -1,12 +1,27 @@
-import 'dart:io';
+/*import 'dart:io';
 
-void main(){
+class sum{
+  void add(){
+    print("this is function");
+  }
+}
+void main() {
   print("hello");
   print("Please enter your name ");
-  var name= stdin.readLineSync();
+  var name = stdin.readLineSync();
   print("hello this is $name how can i help you ");
-}
+  int a = 5;
+  int b = 10;
+  print(b);
+  print(a);
+  void add() {
+    var sum = a + b;
+    print(sum);
+  }
+  var x=new sum();
+  print(x.add());
 
+}*/
 
 
 
